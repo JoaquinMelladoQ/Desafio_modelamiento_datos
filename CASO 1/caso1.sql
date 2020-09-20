d1a14cff9fb0e3ccee0008dda50ebd3b0eccadaa
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-09-20 17:38:34 -0300

    Rut was changed from varchard to numeric

--- CASO 1/caso1.sql
+++ CASO 1/caso1.sql
@@ -2,7 +2,7 @@ CREATE DATABASE modelo;
 
 CREATE TABLE trabajador(
     nombre VARCHAR(50) PRIMARY KEY,
-    rut VARCHAR(50) NOT NULL,
+    rut NUMERIC(50) NOT NULL,
     dirección VARCHAR(100) NOT NULL,
     nombre_dpto VARCHAR(50) FOREIGN KEY,
     liquidación_1_a VARCHAR(200) FOREIGN KEY
