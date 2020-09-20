a965dba03156237a3634d9cdd4246f5dee451635
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-09-20 18:23:41 -0300

    Order of tables reset

--- CASO 1/caso1.sql
+++ CASO 1/caso1.sql
@@ -1,15 +1,5 @@
 CREATE DATABASE modelo;
 
-CREATE TABLE trabajador(
-    nombre VARCHAR(50) NOT NULL PRIMARY KEY,
-    rut NUMERIC(50) NOT NULL,
-    dirección VARCHAR(100) NOT NULL,
-    nombre_dpto VARCHAR(50),
-    liquidación_1_a VARCHAR(200)
-    nombre_trabajador_table VARCHAR(50) REFERENCES departamento (nombre_trabajador_dep)
-    nombre_trabajador_liq VARCHAR(50) REFERENCES liquidaciones (nombre_trabajador_li)
-)
-
 CREATE TABLE departamento(
     nombre_trabajador_dep VARCHAR(50) NOT NULL PRIMARY KEY,
     nombre_dpto VARCHAR(50) NOT NULL,
@@ -18,4 +8,14 @@ CREATE TABLE departamento(
 CREATE TABLE liquidaciones(
     nombre_trabajador_li VARCHAR(50) NOT NULL PRIMARY KEY,
     liquidación_1 VARCHAR(200)
+)
+
+CREATE TABLE trabajador(
+    nombre VARCHAR(50) NOT NULL PRIMARY KEY,
+    rut NUMERIC(50) NOT NULL,
+    dirección VARCHAR(100) NOT NULL,
+    nombre_dpto VARCHAR(50),
+    liquidación_1_a VARCHAR(200)
+    nombre_trabajador_table VARCHAR(50) REFERENCES departamento (nombre_trabajador_dep)
+    nombre_trabajador_liq VARCHAR(50) REFERENCES liquidaciones (nombre_trabajador_li)
 )
