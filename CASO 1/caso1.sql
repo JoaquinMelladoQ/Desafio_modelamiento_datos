ccf4bf9d3ab8142dc37c08a47e44e34321d076c5
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-09-20 18:21:27 -0300

    Fixing keys from table trabajador

--- CASO 1/caso1.sql
+++ CASO 1/caso1.sql
@@ -6,16 +6,16 @@ CREATE TABLE trabajador(
     dirección VARCHAR(100) NOT NULL,
     nombre_dpto VARCHAR(50),
     liquidación_1_a VARCHAR(200)
+    nombre_trabajador_table VARCHAR(50) REFERENCES departamento (nombre_trabajador_dep)
+    nombre_trabajador_liq VARCHAR(50) REFERENCES liquidaciones (nombre_trabajador_li)
 )
 
 CREATE TABLE departamento(
-    nombre_dpto VARCHAR(50) NOT NULL PRIMARY KEY,
-    nombre_trabajador VARCHAR(50) NOT NULL,
-    FOREIGN KEY (nombre_dpto) REFERENCES trabajador (nombre_dpto)
+    nombre_trabajador_dep VARCHAR(50) NOT NULL PRIMARY KEY,
+    nombre_dpto VARCHAR(50) NOT NULL,
 )
 
 CREATE TABLE liquidaciones(
-    liquidación_1 VARCHAR(200) NOT NULL PRIMARY KEY,
-    nombre_trabajador VARCHAR(50) NOT NULL,
-    FOREIGN KEY (liquidación_1) REFERENCES trabajador (liquidación_1_a)
+    nombre_trabajador_li VARCHAR(50) NOT NULL PRIMARY KEY,
+    liquidación_1 VARCHAR(200)
 )
