9e01a78668fbec91df0c4ba69530aec5eeea8023
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-09-20 18:03:34 -0300

    Changing keys from each table

--- CASO 1/caso1.sql
+++ CASO 1/caso1.sql
@@ -4,16 +4,18 @@ CREATE TABLE trabajador(
     nombre VARCHAR(50) PRIMARY KEY,
     rut NUMERIC(50) NOT NULL,
     dirección VARCHAR(100) NOT NULL,
-    nombre_dpto VARCHAR(50) FOREIGN KEY,
-    liquidación_1_a VARCHAR(200) FOREIGN KEY
+    nombre_dpto VARCHAR(50),
+    liquidación_1_a VARCHAR(200)
 )
 
 CREATE TABLE departamento(
-    nombre_dpto VARCHAR(50) PRIMARY KEY
-    nombre_trabajador VARCHAR(50) NOT NULL
+    nombre_dpto VARCHAR(50) PRIMARY KEY,
+    nombre_trabajador VARCHAR(50) NOT NULL,
+    FOREIGN KEY (nombre_dpto) REFERENCES trabajador (nombre_dpto)
 )
 
 CREATE TABLE liquidaciones(
     liquidación_1 VARCHAR(200) PRIMARY KEY,
     nombre_trabajador VARCHAR(50) NOT NULL,
+    FOREIGN KEY (liquidación_1) REFERENCES trabajador (liquidación_1_a)
 )
