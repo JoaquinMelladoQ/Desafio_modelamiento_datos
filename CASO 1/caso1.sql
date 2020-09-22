2c1135ec7d53b393890090b46d5a1dae81173305
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-09-21 23:00:41 -0300

    Removing wrong pdf and refactoring database case 1

--- CASO 1/caso1.sql
+++ CASO 1/caso1.sql
@@ -1,23 +1,22 @@
 CREATE DATABASE modelo;
 
 CREATE TABLE departamento(
-    nombre_trabajador_dep VARCHAR(50) NOT NULL PRIMARY KEY,
-    nombre_dpto VARCHAR(50) NOT NULL,
+    id SERIAL PRIMARY KEY,
+    nombre_trabajador VARCHAR(50) NOT NULL,
 );
 
 CREATE TABLE liquidaciones(
-    nombre_trabajador_li VARCHAR(50) NOT NULL PRIMARY KEY,
-    liquidación_1 VARCHAR(200)
+    id SERIAL PRIMARY KEY,
+    archivo_drive VARCHAR(30),
+    nombre_trabajador INT NOT NULL FOREIGN KEY,
+    REFERENCES trabajador(id)
 );
 
 CREATE TABLE trabajador(
-    nombre VARCHAR(50) NOT NULL PRIMARY KEY,
+    id SERIAL PRIMARY KEY
+    nombre_trabajador VARCHAR(50) NOT NULL,
     rut NUMERIC(50) NOT NULL,
     dirección VARCHAR(100) NOT NULL,
-    nombre_dpto VARCHAR(50),
-    liquidación_1_a VARCHAR(200)
-    nombre_trabajador_table VARCHAR(50)
-    REFERENCES departamento (nombre_trabajador_dep)
-    nombre_trabajador_liq VARCHAR(50)
-    REFERENCES liquidaciones (nombre_trabajador_li)
+    departamento_data INT NOT NULL FOREIGN KEY,
+    REFERENCES departamento(id)
 );
